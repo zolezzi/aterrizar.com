e9055913febd380ce0abacc029e0d6a1eb1b4872
zolezzi <carlos_cardozo@live.com>
2016-04-04 10:38:54 -0300

    Creacion de la column codvalidacion

--- src/main/resources/resources/db/scripts/createtable.sql
+++ src/main/resources/resources/db/scripts/createtable.sql
@@ -3,16 +3,18 @@ CREATE TABLE `aterrizar_schema`.`users` (
   
 										  `nombreUsuario` VARCHAR(45) NOT NULL,
 
-								  		  `Nombre` VARCHAR(45) NOT NULL,
+								  		  `nombre` VARCHAR(45) NOT NULL,
  					   
 										  `apellido` VARCHAR(45) NOT NULL,
  
  					  					  `email` VARCHAR(45) NOT NULL,
  
-				   					 	  `FechaNacimiento` VARCHAR(45) NULL,
+				   					 	  `fechaNacimiento` VARCHAR(45) NULL,
   
 					   					  `contrasenia` VARCHAR(45) NOT NULL,
   					 
 					   					  `validado` TINYINT(1) NULL,
-  
-					    				  PRIMARY KEY (`iduser`, `nombreUsuario`));
+					   					  
+  										  `codvalidacion` VARCHAR(45) NOT NULL,
+  										  
+					    				   PRIMARY KEY (`iduser`, `nombreUsuario`));
